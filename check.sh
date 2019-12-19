76d819eb3388005a2d6df0f08a7197697ea0a2ca
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2019-12-19 18:16:22 +0530

    Add files via upload

---
+++ check.sh
@@ -0,0 +1,59 @@
+pink () {
+	echo "19/06/2020" >> ~/EDU-check/19/06/2020
+	#echo "19/06/2020" >> /sdcard/19062020
+	echo -e -n "\033[96m Enter current date ( dd/mm/yyyy ) "
+	read n
+	if [ $n ];then
+	cd ~/EDU-check
+	rm -f 19/06/2020
+	echo "hi" >> $n
+	fi
+	cd ~/EDU-check
+	if [ -e 19/06/2020 ];then
+	printf "\n\n\033[91m [×] Sorry , your replacement date expire
+	
+	"
+	else
+	printf "\n\n \033[92m Replacement is needed
+	
+	Your replacement date :- 19/12/2019
+	
+	And Replacement expire date :- 19/06/2020
+	
+	Your replacement edu email :- pink@mail.ccsf.edu
+	
+	"
+	fi
+	
+	}
+	cd $PREFIX/bin
+if [ -e EDU ];then
+echo
+else
+printf "\n\033[92m [√] Requirement installing....\n\n"
+apt update
+apt upgrade
+apt install toilet
+apt install figlet
+clear
+printf "\033[92m\n\n [√] Shortcut adding....\n"
+echo "#!/data/data/com.termux/files/usr/bin/sh" >> $PREFIX/bin/EDU
+echo "cd ~/EDU-check" >> $PREFIX/bin/EDU
+echo "bash check.sh" >> $PREFIX/bin/EDU
+chmod 777 $PREFIX/bin/EDU
+clear
+printf "\n Now you can use this command :- EDU\n"
+sleep 5
+fi
+clear
+echo
+figlet EDU | toilet -f term -F gay
+echo
+printf "	\033[91m [ 1 ]\033[92m pink@mail.ccsf.edu
+	\033[91m [ 2 ]\033[92m Exit\n\n\n"
+	echo -e -n "\033[96m Type :- "
+	read a
+	case $a in
+	1)pink ;;
+	*)EDU ;;
+	esac
