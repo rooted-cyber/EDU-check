e2bac5ef3aa378ae6f86d8c3aa50f2eb39f7ca05
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-02-09 20:34:02 +0530

    Add files via upload

--- check.sh
+++ check.sh
@@ -57,6 +57,62 @@ pink () {
 	fi
 	
 	}
+	rashida () {
+	echo "27/06/2020" >> ~/EDU-check/9.08.2020
+	#echo "19/06/2020" >> /sdcard/19062020
+	echo -e -n "\033[96m Enter current date ( dd.mm.yyyy ) "
+	read n
+	if [ $n ];then
+	cd ~/EDU-check
+	rm -f 9.08.2020
+	echo "hi" >> $n
+	fi
+	cd ~/EDU-check
+	if [ -e 9.08.2020 ];then
+	printf "\n\n\033[91m [×] Sorry , your replacement date expire
+	
+	"
+	else
+	printf "\n\n \033[92m Replacement is needed
+	
+	Your replacement date :- 9/02/2020
+	
+	And Replacement expire date :- 9/08/2020
+	
+	Your replacement edu email :- rashida@mail.ccsf.edu
+	
+	"
+	fi
+	
+	}
+	ccsf5 () {
+	echo "7/02/2020" >> ~/EDU-check/7.08.2020
+	#echo "19/06/2020" >> /sdcard/19062020
+	echo -e -n "\033[96m Enter current date ( dd.mm.yyyy ) "
+	read n
+	if [ $n ];then
+	cd ~/EDU-check
+	rm -f 7.08.2020
+	echo "hi" >> $n
+	fi
+	cd ~/EDU-check
+	if [ -e 7.08.2020 ];then
+	printf "\n\n\033[91m [×] Sorry , your replacement date expire
+	
+	"
+	else
+	printf "\n\n \033[92m Replacement is needed
+	
+	Your purchasing date :- 7/2/2020
+	
+	And Replacement expire date :- 7/8/2020
+	
+	Your edu email :- ccsf5@mail.ccsf.edu
+	
+	"
+	fi
+	
+	}
 	
 	
 	cd $PREFIX/bin
@@ -101,14 +157,18 @@ update () {
 	}
 printf "	\033[91m [ 1 ]\033[92m pink@mail.ccsf.edu
 	\033[91m [ 2 ]\033[92m buy1@mail.ccsf.edu
-	\033[91m [ 3 ]\033[92m Update tool
-	\033[91m [ 4 ]\033[92m Exit\n\n\n"
+	\033[91m [ 3 ]\033[92m ccsf5@mail.ccsf.edu
+	\033[91m [ 4 ]\033[92m rashida@mail.ccsf.edu
+	\033[91m [ 5 ]\033[92m Update tool
+	\033[91m [ 6 ]\033[92m Exit\n\n\n"
 	echo -e -n "\033[96m Type :- "
 	read a
 	case $a in
 	1)pink ;;
 	2)buy1 ;;
-	3)update ;;
-	4)exit 0 ;;
+	3)ccsf5 ;;
+	4)rashida ;;
+	5)update ;;
+	6)exit 0 ;;
 	*)EDU ;;
 	esac
